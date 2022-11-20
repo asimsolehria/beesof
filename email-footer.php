a948b3f61421e94a48e5d017b84d1486d727f15b
Asim Habib <de.asimhabib@gmail.com>
2022-11-20 22:53:52 +0500

    Added woocommerce default email files

---
+++ email-footer.php
@@ -0,0 +1,187 @@
+<?php
+
+/**
+ * Email Footer
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-footer.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 3.7.0
+ */
+
+defined('ABSPATH') || exit;
+?>
+<tr>
+	<td class="box box-ext cx-info-box" style="background: #fff;padding: 0 25px;text-align: center;color: #2a3376;">
+		<table class="info-cta" style="margin: 0 auto;">
+			<tbody>
+				<tr>
+					<td class="em-spacer-5" style="height: 32px;text-align: center;">
+					</td>
+				</tr>
+				<tr>
+					<td class="title" style="text-align: center;font-weight: 700;text-transform: uppercase;font-size: 19px;padding-bottom: 18px;">
+						Questions? We're on call. </td>
+				</tr>
+				<tr>
+					<td class="info" style="text-align: center;">
+						Monday to Friday 9am - 9pm ET </td>
+				</tr>
+				<tr>
+					<td class="info" style="text-align: center;">
+						Saturday to Sunday 10am - 6pm ET </td>
+				</tr>
+				<tr>
+					<td class="links" style="text-align: center;padding-top: 20px;">
+						<img src="https://forensichealing.com/wp-content/uploads/2022/11/call.png" class="icon-bubble" style="width: 28px;margin-bottom: -9px;margin-right: 3px;">
+						+61 (2) 8069 5881 <span class="pipe">
+							&nbsp;|&nbsp;
+						</span>
+						<img src="https://forensichealing.com/wp-content/uploads/2022/11/support.png" class="icon-email" style="margin-bottom: -6px;margin-right: 1px;width: 28px;">
+						support@casper.com
+					</td>
+				</tr>
+				<tr>
+					<td class="em-spacer-5" style="height: 32px;text-align: center;">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</td>
+</tr>
+
+<tr>
+	<td class="em-spacer-1" style="height: 26px;"></td>
+</tr>
+<tr>
+	<td class="box box-ext referral-box" style="background: #FCDB67;padding: 0 25px;text-align: center;color: #000000;font-family: Helvetica">
+		<table class="referral-info" style="margin: 0 auto;">
+			<tbody>
+				<tr>
+					<td class="em-spacer-1" style="height: 26px;text-align: center;">
+					</td>
+				</tr>
+				<tr align="center">
+					<td class="friends-img-wrap" style="padding-bottom: 8px;text-align: center;">
+						<a href="https://casper.com/anon/friends?utm_campaign=order+confirmation&amp;utm_content=thank+you+for+your+order&amp;utm_medium=email&amp;utm_source=casper+support" target="_blank" style="color: #fff;">
+							<img src="https://forensichealing.com/wp-content/uploads/2022/11/pin.png" width="68">
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td class="title" style="text-align: center;padding-bottom: 10px;font-weight: 700;text-transform: uppercase;font-size: 19px;">
+						POSTAL ADDRESS </td>
+				</tr>
+				<tr>
+					<td class="info" style="text-align: center;">
+						6/399 Belmore Road
+						Suite 115
+						Balwyn VIC 3103 Australia. </td>
+				</tr>
+				<tr>
+					<td class="em-spacer-0" style="height: 20px;text-align: center;">
+					</td>
+				</tr>
+				<tr>
+					<td class="link" style="text-align: center;">
+						<a href="https://forensichealing.com/contact/" target="_blank" style="color: #000000;">Contact Us</a>
+					</td>
+				</tr>
+				<tr>
+					<td class="em-spacer-1" style="height: 26px;text-align: center;">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</td>
+</tr>
+
+
+</tbody>
+</table>
+<table style="margin: 0 auto;">
+	<tbody>
+		<tr>
+			<td class="em-spacer-2" style="height: 52px;"></td>
+		</tr>
+		<tr>
+			<td class="footer footer-1" style="color: #999;font-size: 13px;">
+				+1 888–498–0003 <span class="pipe">
+					&nbsp;|&nbsp;
+				</span>
+				support@casper.com</td>
+		</tr>
+		<tr>
+			<td class="em-spacer-0" style="height: 20px;"></td>
+		</tr>
+		<tr>
+			<td align="center" class="footer footer-2" style="color: #999;font-size: 13px;">
+				<table class="footer-icons" style="margin: 0 auto;width: 153px;">
+					<tbody>
+						<tr>
+							<td class="footer-icon">
+								<a href="https://www.facebook.com/ForensicHealing" target="_blank" style="color: #0047be;">
+									<img src="https://forensichealing.com/wp-content/uploads/2022/11/facebook.png" width="31" height="31">
+								</a>
+							</td>
+							<td class="footer-icon">
+								<a href="https://www.instagram.com/marisarussoforensichealing/" target="_blank" style="color: #0047be;">
+									<img src="https://forensichealing.com/wp-content/uploads/2022/11/instagram.png" width="31" height="31">
+								</a>
+							</td>
+							<td class="footer-icon">
+								<a href="https://twitter.com/marisarusso" target="_blank" style="color: #0047be;">
+									<img src="https://forensichealing.com/wp-content/uploads/2022/11/twitter.png" width="31" height="31">
+								</a>
+							</td>
+							<td class="footer-icon">
+								<a href="https://au.linkedin.com/in/marisarusso" target="_blank" style="color: #0047be;">
+									<img src="https://forensichealing.com/wp-content/uploads/2022/11/linkedin.png" width="31" height="31">
+								</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+
+
+
+<table style="margin: 0 auto;">
+	<tbody>
+		<tr>
+			<td class="copyright-footer" style="padding-top: 15px;padding-bottom: 20px;font-size: 11px;color: #b2b2b2;">
+				1100 High Street
+				Armadale
+				VIC 3143 Australia <span class="pipe">
+					&nbsp;|&nbsp;
+				</span>
+				© Copyright 2009-2020 Forensic Healing™ All Rights Reserved.. </td>
+		</tr>
+	</tbody>
+</table>
+
+<!-- class content -->
+
+
+
+
+</body>
+
+
+</body>
+
+</html>
